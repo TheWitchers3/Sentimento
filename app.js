d338ab640c11b83a54d1f1c21f6c4d4831b90108
Ashlesh Mahule <ashleshmahule15@gmail.com>
2020-05-08 17:25:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,78 @@
-import dotenv
-import os
-import sys
-from flask import Flask
-from flask import request
-from flask import jsonify
-import tsaFinal
-from flask_cors import CORS
+require("dotenv").config();
 
-dotenv.load_dotenv()
+var cors = require('cors')
+var express = require("express"),
+  request = require("request"),
+  bodyParser = require("body-parser"),
+  app = express();
+  app.use(cors())
 
-CK = os.getenv('CONSUMER_KEY')
-CS = os.getenv('CONSUMER_SECRET')
-AT = os.getenv('ACCESS_TOKEN')
-ATS = os.getenv('ACCESS_TOKEN_SECRET')
+const http = require('http');
 
-app = Flask(__name__)
-CORS(app)
+const hostname = '127.0.0.1';
+const port = 3000;
 
-@app.route('/')
-def home():
-    return "Welcome"
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
-@app.route('/getTrending')
-def getTrending():
-    d=tsaFinal.getTrends()
-    return d
+var seeds = require("./seeds.js");
 
-@app.route('/notif')
-def getNotified():
-    d=tsaFinal.getNotifyTrends()
-    return jsonify(d)
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-@app.route('/analyze')
-def analyze():
-    query = request.args.get('query')
-    print(query)
-    tweetsjson = tsaFinal.getAnalysis(query,ck=CK, cs=CS, at=AT, ats=ATS)
-    res=tsaFinal.supreme(query)
-    print("...tweets fetched!")
-    print(tweetsjson)
-    if (tweetsjson == None or (not tweetsjson)):
-        errors = []
-        errors.append({'text': "something went wrong!"})
-        return jsonify(errors)
-    return jsonify(tweetsjson)
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/twitteranalysis", (req, res) => {
+  res.render("twitter");
+  
+});
 
 
-if (__name__ == "__main__"):
-    app.run()
+// var interval = setInterval(function () {
+
+//   request(
+//     "https://twittersentimentanalyticsback.herokuapp.com/notif",
+//     function (err, response, body) {
+//       if (!err && response.statusCode === 200) {
+//         var notif = JSON.parse(body);
+//         console.log(notif);
+//       }
+//     }
+//   );
+// }, 3000);
+
+
+app.get("/analyze", (req, res) => {
+  var query = req.query;
+  var count = req.query.ntwts;
+  console.log(query['searchtrend'])
+
+  request(
+    "https://twittersentimentanalyticsback.herokuapp.com/analyze?query=" + query['searchtrend'],
+    function(err, response, body) {
+      if (!err && response.statusCode === 200) {
+        var tweetsAnalysis = JSON.parse(body);
+        console.log(tweetsAnalysis);
+        res.json(tweetsAnalysis);
+      }
+    }
+  );
+
+  // to get data from the seeds file for testing purposes
+  // var tweetAnalysis = seeds;
+  // res.json(tweetAnalysis);
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log("Server started!!");
+});
